60dcd0908543955d320f83f08e4b4e4ea6036c7c
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-06-27 13:45:10 -0300

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+const rootEl = document.getElementById("root");
+const root = createRoot(rootEl);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
